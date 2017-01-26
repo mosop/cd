90fc13edc9339ecbeaefbdd8124b4c73edda15c4
mosop <mosop@users.noreply.github.com>
2017-01-26 21:06:39 +0900

    fix spec

--- spec/code_samples/searching_underlying_entries_spec.cr
+++ spec/code_samples/searching_underlying_entries_spec.cr
@@ -4,13 +4,13 @@ module CdCodeSamples::SearchingUnderlyingEntries
   it name do
     Cd.into(__DIR__, "searching_underlying_entries") do |foo|
       Stdio.capture do |io|
-        foo.glob("src/**/*.cr") do |entry|
+        foo.glob("src/**/*.cr").sort{|a,b| a.path <=> b.path}.each do |entry|
           puts entry
         end
         io.out.gets_to_end.chomp.should eq <<-EOS
+        #{foo}/src/foo.cr
         #{foo}/src/foo/bar.cr
         #{foo}/src/foo/version.cr
-        #{foo}/src/foo.cr
         EOS
       end
     end
